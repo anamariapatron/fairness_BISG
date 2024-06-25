0e22d3a3834a1e75cf6cb95c7c8ec4edf88625ee
anamariapatron <83187586+anamariapatron@users.noreply.github.com>
2024-06-25 16:50:21 -0400

    Update code_cascade_only_surname.R

--- main/hierarchical/code_cascade_only_surname.R
+++ main/hierarchical/code_cascade_only_surname.R
@@ -60,7 +60,7 @@ set.seed(123)
 # for full sample run this 
 muestra <- df_no_territories
 
-muestra$GEOID <- paste0(muestra$state_code, muestra$county) 
+muestra$GEOID <- paste0(muestra$state_code, muestra$county, muestra$tract) 
 muestra$surname <- toupper(muestra$surname)
 
 # load database surnames
@@ -80,7 +80,7 @@ muestra <- merge(muestra, surnames2010, by = "surname", all.x = TRUE)
 
 #colnames(muestra)[colnames(muestra) == "geo_id"] <- "GEOID"
 # paste p(G_i| R_i ) from census- counts
-subset_census <- subset(census, select = c("GEOID","r_whi", "r_bla", "r_asi", "r_his","r_oth"))
+subset_census <- subset(census_final, select = c("GEOID","r_whi", "r_bla", "r_asi", "r_his","r_oth","r_nonwhi","r_nonbla","r_nonasi","r_nonhis"))
 names(subset_census)[names(subset_census) == "GEOID"] <- "GEOID_tract"
 muestra <- merge(muestra, subset_census, by = "GEOID_tract", all.x = TRUE)
 
@@ -89,7 +89,6 @@ muestra <- merge(muestra, subset_census, by = "GEOID_tract", all.x = TRUE)
 # ----------------------
 # first stage: white vs non white
 muestra$p_nonwhi <- muestra$p_bla + muestra$p_his + muestra$p_asi + muestra$p_oth
-muestra$r_nonwhi <- muestra$r_bla + muestra$r_his + muestra$r_asi + muestra$r_oth
 muestra$pred_whi <- muestra$p_whi* muestra$r_whi
 muestra$pred_nonwhi <- muestra$p_nonwhi* muestra$r_nonwhi
 denominator <- rowSums(muestra[, c("pred_nonwhi", "pred_whi")])         
@@ -98,7 +97,6 @@ muestra$pred_nonwhi <-muestra$pred_nonwhi/denominator
 
 # second stage: asian vs non asian
 muestra$p_nonasi <- muestra$p_bla + muestra$p_his + muestra$p_oth
-muestra$r_nonasi <- muestra$r_bla + muestra$r_his + muestra$r_oth
 muestra$pred_asi <- muestra$p_asi* muestra$r_asi
 muestra$pred_nonasi <- muestra$p_nonasi* muestra$r_nonasi
 denominator <- rowSums(muestra[, c("pred_nonasi", "pred_asi")])         
@@ -107,7 +105,6 @@ muestra$pred_nonasi <-muestra$pred_nonasi/denominator
 
 # third stage: black vs non black
 muestra$p_nonbla <-   muestra$p_his + muestra$p_oth
-muestra$r_nonbla <-  muestra$r_his + muestra$r_oth
 muestra$pred_bla <- muestra$p_bla* muestra$r_bla
 muestra$pred_nonbla <- muestra$p_nonbla* muestra$r_nonbla
 denominator <- rowSums(muestra[, c("pred_nonbla", "pred_bla")])         
@@ -116,7 +113,6 @@ muestra$pred_nonbla <-muestra$pred_nonbla/denominator
 
 # fourth stage: hispanic vs non hispanic
 muestra$p_nonhis <-  muestra$p_oth
-muestra$r_nonhis <-  muestra$r_oth
 muestra$pred_his <- muestra$p_his* muestra$r_his
 muestra$pred_nonhis <- muestra$p_nonhis* muestra$r_nonhis
 denominator <- rowSums(muestra[, c("pred_nonhis", "pred_his")])         
